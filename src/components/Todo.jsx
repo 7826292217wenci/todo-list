63eafb4a299392130db40784b24e9ab49401c0f8
7826292217wenci <2509564209@qq.com>
2024-04-09 02:59:21 +0800

    v6

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -19,7 +19,7 @@ function Todo(props) {
 
   const [isEditing, setEditing] = useState(false);
   const [newName, setNewName] = useState("");
-//Tap can move diffierent button , enter is click
+
   const editFieldRef = useRef(null);
   const editButtonRef = useRef(null);
 
@@ -36,6 +36,13 @@ function Todo(props) {
     setNewName("");
     setEditing(false);
   }
+
+  const handleMapLinkClick = (event, latitude, longitude) => {
+    event.preventDefault();
+    const mapURL = `https://www.openstreetmap.org/?mlat=${latitude}&mlon=${longitude}#map=18/${latitude}/${longitude}`;
+    window.location.href = mapURL;
+  };
+
 
   const editingTemplate = (
     <form className="stack-small" onSubmit={handleSubmit}>
@@ -79,149 +86,139 @@ function Todo(props) {
         />
         <label className="todo-label" htmlFor={props.id}>
           {props.name}
-          <a href={props.location.mapURL}>(map)</a> 
+          &nbsp; | &nbsp;
+          <a href="#" onClick={(event) => handleMapLinkClick(event, props.location.latitude, props.location.longitude)}>Map</a>
           &nbsp; | &nbsp; 
           <a href={props.location.smsURL}>(sms)</a>                    
         </label>
       </div>
       <div className="btn-group">
         <button
-          type="button"
-          className="btn"
-          onClick={() => 
-            setEditing(true)}
-            ref={editButtonRef}>
+          type="button" 
+          className="btn" 
+          onClick={() => {
+            setEditing(true);
+          }}
+          ref={editButtonRef}>
           Edit <span className="visually-hidden">{props.name}</span>
         </button>
-        <Popup                                     
-          trigger={ 
-            <button type="button" className="btn"> 
-              {" "} 
-              Take Photo{" "} 
-            </button> 
-          } 
-          modal 
-        > 
-          <div> 
-            <WebcamCapture id={props.id} photoedTask={props.photoedTask} /> 
-          </div> 
-        </Popup> 
- 
-        <Popup                                   
-          trigger={ 
-            <button type="button" className="btn"> 
-              {" "} 
-              View Photo{" "} 
-            </button> 
-          } 
-          modal 
-        > 
-          <div> 
-            <ViewPhoto id={props.id} alt={props.name} /> 
-          </div> 
-        </Popup>        
-      </div>
-      <button
+        <Popup
+          trigger={
+            <button type="button" className="btn">
+              {" "}
+              Take Photo{" "}
+            </button>
+          }
+          modal
+        >
+          <div>
+            <WebcamCapture id={props.id} photoedTask={props.photoedTask} />
+            </div>
+        </Popup>
+
+        <Popup
+          trigger={
+            <button type="button" className="btn">
+              {" "}
+              View Photo{" "}
+            </button>
+          }
+          modal
+        >
+          <div>
+            <ViewPhoto id={props.id} alt={props.name} />
+          </div>
+        </Popup>
+
+        <button
           type="button"
           className="btn btn__danger"
           onClick={() => props.deleteTask(props.id)}>
           Delete <span className="visually-hidden">{props.name}</span>
-        </button>      
+        </button>
+
+      </div>
     </div>
   );
-
-    useEffect(() => {
+  useEffect(() => {
     if (!wasEditing && isEditing) {
       editFieldRef.current.focus();
     } else if (wasEditing && !isEditing) {
       editButtonRef.current.focus();
     }
   }, [wasEditing, isEditing]);
-  
 
   return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 }
 
-
-const WebcamCapture = (props) => {  
-  const webcamRef = useRef(null); 
-  const [imgSrc, setImgSrc] = useState(null); 
-  const [imgId, setImgId] = useState(null); 
+const WebcamCapture = (props) => {
+  const webcamRef = useRef(null);
+  const [imgSrc, setImgSrc] = useState(null);
+  const [imgId, setImgId] = useState(null);
   const [photoSave, setPhotoSave] = useState(false);
 
-  useEffect(() => {  
-    if (photoSave) { 
-      console.log("useEffect detected photoSave"); 
-      props.photoedTask(imgId); 
-      setPhotoSave(false); 
-    } 
-  }); 
+  useEffect(() => {
+    if (photoSave) {
+      console.log("useEffect detected photoSave");
+      props.photoedTask(imgId);
+      setPhotoSave(false);
+    }
+  });
   console.log("WebCamCapture", props.id);
 
-  const capture = useCallback( (id) => { 
-    const imageSrc = webcamRef.current.getScreenshot(); 
-    setImgSrc(imageSrc); 
-    console.log("capture", imageSrc.length, id); 
-  }, 
-  [webcamRef, setImgSrc] 
+  const capture = useCallback( 
+    (id) => {
+      const imageSrc = webcamRef.current.getScreenshot();
+      setImgSrc(imageSrc);
+      console.log("capture", imageSrc.length, id);
+    },
+    [webcamRef, setImgSrc]
   );
 
-  const savePhoto = (id, imgSrc) => { 
-    console.log("savePhoto", imgSrc.length, id); 
+  const savePhoto = (id, imgSrc) => {
+    console.log("savePhoto", imgSrc.length, id);
     addPhoto(id, imgSrc);
-    //props.photoedTask(id); 
-    setImgId(id); 
-    setPhotoSave(true); 
+    setImgId(id);
+    setPhotoSave(true);
   };
 
-  const cancelPhoto = (id, imgSrc) => { 
-    if (!imgSrc) {
-      console.log("imgSrc is empty");
-      return;
-    }else{
-      console.log("cancelPhoto", imgSrc.length, id);
-    }
-    
-    setReturnToMain(true);
-    if (returnToMain) {
-      return <Redirect to="/" />;
-    }
+  const cancelPhoto = (id, imgSrc) => {
+    console.log("cancelPhoto", imgSrc.length, id);
   };
-  return ( 
-    <> 
-      {!imgSrc && ( 
-        <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" /> 
-      )} 
+
+  return (
+    <>
+      {!imgSrc && (
+        <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+      )}
       {imgSrc && <img src={imgSrc} />} 
-      <div className="btn-group"> 
-        {!imgSrc && (  
-          <button 
-            type="button" 
-            className="btn" 
-            onClick={() => capture(props.id)}> 
-            Capture photo 
-          </button> 
-        )} 
-        {imgSrc && (   
-          <button 
-            type="button" 
-            className="btn" 
-            onClick={() => savePhoto(props.id, imgSrc)}> 
-            Save Photo 
-          </button> 
-        )} 
-        <button         
-          type="button" 
-          className="btn todo-cancel" 
-          onClick={() => cancelPhoto(props.id, imgSrc)}> 
-          Cancel 
-        </button> 
-      </div> 
+      <div className="btn-group">
+        {!imgSrc && (
+          <button
+            type="button"
+            className="btn"
+            onClick={() => capture(props.id)}>
+            Capture photo
+          </button>
+        )}
+        {imgSrc && (
+          <button
+            type="button"
+            className="btn"
+            onClick={() => savePhoto(props.id, imgSrc)}>
+            Save Photo
+          </button>
+        )}
+        <button
+          type="button"
+          className="btn todo-cancel"
+          onClick={() => cancelPhoto(props.id, imgSrc)}>
+          Cancel
+        </button>
+      </div>
     </>
   );
-
 };
-
 const ViewPhoto = (props) => {
   const [photoSrc, setPhotoSrc] = useState(null);
 
